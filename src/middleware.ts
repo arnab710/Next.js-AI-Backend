8afda92472d8c1857acf1215f65eb12d169f3db6
Arnab Dey <arnabdey710@gmail.com>
2023-11-10 02:10:54 +0530

    Adding Middleware for CORS

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export function middleware() {
+	// retrieving the current response
+	const res = NextResponse.next();
+
+	// adding the CORS headers to the response
+	res.headers.append("Access-Control-Allow-Credentials", "true");
+	res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+	res.headers.append("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+	res.headers.append("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+
+	return res;
+}
